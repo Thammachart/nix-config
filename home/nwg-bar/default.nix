77490efd8a540d9de1631bf2c5a0d53053c3ef6a
Thammachart <1731496+Thammachart@users.noreply.github.com>
2024-01-28 16:37:43 +0700

    nwgbarPath

--- home/nwg-bar/default.nix
+++ home/nwg-bar/default.nix
@@ -1,9 +1,9 @@
 { pkgs, templateFile, isDesktop, homeConfig, ...  }:
 
 let
-  settings = { nwg-bar-path = pkgs.nwg-bar; };
+  settings = { nwgbarPath = pkgs.nwg-bar.outPath; };
 in
 {
-  home.file.".config/nwg-bar/exit-bar/bar.json.tmpl".source = templateFile "nwg-bar-json-${homeConfig.username}" ./exit-bar/bar.json.tmpl settings;
+  home.file.".config/nwg-bar/exit-bar/bar.json".source = templateFile "nwg-bar-json-${homeConfig.username}" ./exit-bar/bar.json.tmpl settings;
   home.file.".config/nwg-bar/exit-bar/style.css".source = ./exit-bar/style.css;
 }
