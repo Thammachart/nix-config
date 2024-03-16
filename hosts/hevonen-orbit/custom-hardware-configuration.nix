86f8fd1d7e8dd02fc3fab63cac33cab34e1ed226
Thammachart <1731496+Thammachart@users.noreply.github.com>
2024-03-16 23:22:17 +0700

    hevonen-orbit installed

--- hosts/hevonen-orbit/custom-hardware-configuration.nix
+++ hosts/hevonen-orbit/custom-hardware-configuration.nix
@@ -1,2 +1,19 @@
-{}:
-{}
+{ config, lib, pkgs, pkgs-stable, modulesPath, ... }:
+
+{
+  boot.initrd.kernelModules = [ "amdgpu" ];
+
+  fileSystems."/".options = [ "noatime" "commit=120" "compress=zstd:1" "space_cache=v2" "subvol=@" ];
+
+  fileSystems."/home".options = [ "noatime" "commit=120" "compress=zstd:1" "space_cache=v2" "subvol=@home" ];
+
+  fileSystems."/nix".options = [ "noatime" "commit=120" "compress=zstd:1" "space_cache=v2" "subvol=@nix" ];
+
+  fileSystems."/boot".options = [ "noatime" ];
+
+  hardware.opengl = {
+    enable = true;
+
+    driSupport = true;
+  };
+}
