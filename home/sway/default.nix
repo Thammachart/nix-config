7f17ec84f86ded0ec6ee0eaf9c0bc0389b0129ab
Thammachart <1731496+Thammachart@users.noreply.github.com>
2024-03-20 13:02:02 +0700

    swaybar status

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -6,4 +6,5 @@
   home.file.".config/sway/autostart".source = templateFile "sway-autostart-${configData.username}" ./autostart.tmpl configData.homeSettings;
   home.file.".config/sway/inputs".source = templateFile "sway-inputs-${configData.username}" ./inputs.tmpl { isDesktop = isDesktop; };
   home.file.".config/sway/theme".source = ./theme;
+  home.file.".config/sway/status.sh".source = ./status.sh;
 }
