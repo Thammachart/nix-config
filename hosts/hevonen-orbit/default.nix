75689202f4675dfd9b22f5d44a55067c3eee156a
Thammachart <1731496+Thammachart@users.noreply.github.com>
2024-08-01 22:45:49 +0700

    flake.lock updated

--- hosts/hevonen-orbit/default.nix
+++ hosts/hevonen-orbit/default.nix
@@ -14,7 +14,7 @@ in
       ../../modules/system.nix
     ];
     
-  boot.kernelPackages = pkgs.linuxPackages_cachyos-lto;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   users.users."${configData.username}".extraGroups = ["docker"];
 
