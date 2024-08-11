afc1d9664559f7502389142d5acd53801cec76a2
Thammachart <1731496+Thammachart@users.noreply.github.com>
2024-08-12 01:09:24 +0700

    river inputs

--- home/river/default.nix
+++ home/river/default.nix
@@ -1,8 +1,9 @@
 { pkgs, templateFile, configData, isDesktop, ...  }:
 
 {
-  home.file.".config/river/autostart".source = ./autostart;
   home.file.".config/river/variables".source = ./variables;
+  home.file.".config/river/autostart".source = ./autostart;
+  home.file.".config/river/inputs".source = templateFile "river-inputs-${configData.username}" ./inputs.tmpl { isDesktop = isDesktop; };
 
   wayland.windowManager.river = {
     enable = true;
