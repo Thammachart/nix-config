2809b89200894b5a715710ae09d7a5ca87382fc5
Thammachart <1731496+Thammachart@users.noreply.github.com>
2024-08-12 01:21:42 +0700

    river w/ template

--- home/river/default.nix
+++ home/river/default.nix
@@ -1,8 +1,8 @@
 { pkgs, templateFile, configData, isDesktop, ...  }:
 
 {
-  home.file.".config/river/variables".source = ./variables;
-  home.file.".config/river/autostart".source = ./autostart;
+  home.file.".config/river/variables".source = templateFile "river-variables-${configData.username}" ./variables.tmpl configData.homeSettings;
+  home.file.".config/river/autostart".source = templateFile "river-autostart-${configData.username}" ./autostart.tmpl configData.homeSettings;
   home.file.".config/river/inputs".source = templateFile "river-inputs-${configData.username}" ./inputs.tmpl { isDesktop = isDesktop; };
 
   wayland.windowManager.river = {
