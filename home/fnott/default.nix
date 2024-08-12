2cb2dc15af268454135f718e68f0d14943c4c726
Thammachart <1731496+Thammachart@users.noreply.github.com>
2024-08-13 02:01:49 +0700

    fnott config

--- home/fnott/default.nix
+++ home/fnott/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}:
+{pkgs, templateFile, configData, ...}:
 {
   home.file.".config/fnott/fnott.ini".source = templateFile "fnott-config-${configData.username}" ./fnott.ini.tmpl configData.homeSettings;
 }
