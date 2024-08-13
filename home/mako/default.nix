2b00e3ff832988fc37e1406f747a054d47acf0b2
Thammachart <1731496+Thammachart@users.noreply.github.com>
2024-08-13 17:02:53 +0700

    mako icon

--- home/mako/default.nix
+++ home/mako/default.nix
@@ -1,4 +1,8 @@
 {pkgs, templateFile, configData, ...}:
 {
-  home.file.".config/mako/config".source = templateFile "mako-config-${configData.username}" ./config.tmpl configData.homeSettings;
+  home.file.".config/mako/config".text = ''
+  icons=1
+  icon-path=${pkgs.papirus-icon-theme}/share/icons/Papirus
+  font=${configData.homeSettings.fonts.latin.ui}, 12
+  '';
 }
