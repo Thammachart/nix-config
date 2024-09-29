8a3f5f446b614f59b94fc2a0a121c85bfb8d0058
Thammachart <1731496+Thammachart@users.noreply.github.com>
2024-09-29 22:27:52 +0700

    logind.lidSwitch for server

--- modules/system.nix
+++ modules/system.nix
@@ -344,6 +344,8 @@ in
     }
   ];
 
+  services.logind.lidSwitch = if conditions.isServer then "lock" else "suspend";
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
