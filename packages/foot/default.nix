08245fba7aaa12a4f26157fc7d323eb604986048
Thammachart <1731496+Thammachart@users.noreply.github.com>
2024-11-05 10:25:55 +0700

    misc: attributions to foot patch

--- packages/foot/default.nix
+++ packages/foot/default.nix
@@ -3,6 +3,7 @@ pkgs.foot.overrideAttrs (final: pre: {
   pname = "foot-with-patches";
 
   patches = [
+    ## Courtesy to https://codeberg.org/dnkl/foot/pulls/1267
     ./001-output-text-binding.patch
   ];
 })
