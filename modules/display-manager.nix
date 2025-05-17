2e3a3bf7a5d62fd845f0ecf763e55d8b1757347b
Thammachart <1731496+Thammachart@users.noreply.github.com>
2025-05-18 03:32:57 +0700

    initial session

--- modules/display-manager.nix
+++ modules/display-manager.nix
@@ -14,7 +14,7 @@ lib.mkIf conditions.graphicalUser {
       #   user = "greeter";
       # };
 
-      initial_session = lib.mkIf (!conditions.isDesktop) {
+      initial_session = {
         command = "${compositor.launch}/bin/launch";
         user = configData.username;
       };
