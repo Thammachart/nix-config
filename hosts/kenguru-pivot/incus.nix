3a1d5d17babd56dbf26c37ea133c383df4950cc5
Thammachart <1731496+Thammachart@users.noreply.github.com>
2025-07-27 01:43:32 +0700

    incus profiles and projects

--- hosts/kenguru-pivot/incus.nix
+++ hosts/kenguru-pivot/incus.nix
@@ -1,6 +1,21 @@
 { lib, config, configData, hostConfig, pkgs, conditions, ... }:
 let
   defaultIncusBridge = hostConfig.networking.incusBridgeIfname;
+  defaultProfileSetting = {
+    devices = {
+      eth0 = {
+        name = "eth0";
+        network = defaultIncusBridge;
+        type = "nic";
+      };
+      root = {
+        path = "/";
+        pool = "local";
+        size = "60GiB";
+        type = "disk";
+      };
+    };
+  };
 in
 {
   virtualisation.incus = {
@@ -10,50 +25,22 @@ in
       networks = [
         {
           config = {
-            "ipv4.address" = "10.100.100.1/24";
+            "ipv4.address" = "10.100.0.1/16";
             "ipv4.nat" = "true";
+            "ipv6.address" = "fda0::1/96";
+            "ipv6.nat" = "true";
           };
           name = defaultIncusBridge;
           type = "bridge";
         }
       ];
       projects = [
-        { name = "default"; } { name = "local-k8s"; }
+        { name = "default"; } { name = "local-k8s"; description = "K8s"; } { name = "local-k3s"; description = "K3s"; }
       ];
       profiles = [
-        {
-          devices = {
-            eth0 = {
-              name = "eth0";
-              network = defaultIncusBridge;
-              type = "nic";
-            };
-            root = {
-              path = "/";
-              pool = "local";
-              size = "60GiB";
-              type = "disk";
-            };
-          };
-          name = "default";
-        }
-        {
-          devices = {
-            eth0 = {
-              name = "eth0";
-              network = defaultIncusBridge;
-              type = "nic";
-            };
-            root = {
-              path = "/";
-              pool = "local";
-              size = "60GiB";
-              type = "disk";
-            };
-          };
-          project = "local-k8s";
-          name = "default";
-        }
+        (defaultProfileSetting // { project = "default"; name = "default"; })
+        (defaultProfileSetting // { project = "local-k8s"; name = "k8s"; })
+        (defaultProfileSetting // { project = "local-k3s"; name = "k3s"; })
       ];
       storage_pools = [
         {
