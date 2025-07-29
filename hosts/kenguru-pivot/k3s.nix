661b9cdf6849b6343afc3eea2bd779ed8b4724c7
Thammachart <1731496+Thammachart@users.noreply.github.com>
2025-07-30 02:33:26 +0700

    k3s extra flags

--- hosts/kenguru-pivot/k3s.nix
+++ hosts/kenguru-pivot/k3s.nix
@@ -2,12 +2,24 @@
 let
 in
 lib.mkIf conditions.k3s {
+  networking.firewall.allowedTCPPorts = [
+    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+    2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+  ];
+  networking.firewall.allowedUDPPorts = [
+    8472 # k3s, flannel: required if using multi-node for inter-node networking
+  ];
+
   services.k3s = {
     enable = true;
     clusterInit = true;
+    role = "server";
     extraFlags = [
-      "--cluster-cidr=10.42.0.0/16,fd09:42::/56"
-      "--service-cidr=10.43.0.0/16,fd09:43::/112"
+      "--write-kubeconfig-mode=644"
+      "--cluster-cidr=10.50.0.0/16,fd09:50::/56"
+      "--service-cidr=10.51.0.0/16,fd09:51::/112"
+      "--cluster-dns=10.51.0.10,fd09:51::a"
       "--flannel-ipv6-masq"
     ];
   };
