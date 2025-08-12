03e12e348818183c6e5b1134b15be90d1d73501c
Thammachart <1731496+Thammachart@users.noreply.github.com>
2025-08-13 04:00:43 +0700

    flake.lock updated, k8s metric server

--- hosts/kenguru-pivot/k3s.nix
+++ hosts/kenguru-pivot/k3s.nix
@@ -6,6 +6,7 @@ lib.mkIf conditions.k3s {
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
     2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
     2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+    10250 # k8s metric server
   ];
   networking.firewall.allowedUDPPorts = [
     8472 # k3s, flannel: required if using multi-node for inter-node networking
