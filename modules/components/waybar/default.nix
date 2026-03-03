d898786de61b081a1c5ea3391d2bc9f24e0c1dc0
Thammachart <1731496+Thammachart@users.noreply.github.com>
2026-03-03 18:10:29 +0700

    waybar hyprland left vertical bar

--- modules/components/waybar/default.nix
+++ modules/components/waybar/default.nix
@@ -16,7 +16,7 @@
     home.file.".config/waybar/sway.json".source = config.templateFile "waybar-sway-json" ./sway.json.tmpl settings;
 
     home.file.".config/waybar/hyprland.css".source = config.templateFile "waybar-hyprland-css" ./hyprland.css.tmpl settings;
-    home.file.".config/waybar/hyprland.json".source = config.templateFile "waybar-hyprland-json" ./hyprland.json.tmpl settings;
+    home.file.".config/waybar/hyprland.jsonc".source = config.templateFile "waybar-hyprland-json" ./hyprland.jsonc.tmpl settings;
 
     home.file.".config/waybar/river.css".source = config.templateFile "waybar-river-css" ./river.css.tmpl settings;
     home.file.".config/waybar/river.json".source = config.templateFile "waybar-river-json" ./river.json.tmpl settings;
