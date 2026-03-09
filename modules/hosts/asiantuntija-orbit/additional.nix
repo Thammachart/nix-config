3d6d221faf864b011c46b8885f2384df81b19543
Thammachart <1731496+Thammachart@users.noreply.github.com>
2026-03-09 15:46:21 +0700

    flake.lock updated

--- modules/hosts/asiantuntija-orbit/additional.nix
+++ modules/hosts/asiantuntija-orbit/additional.nix
@@ -4,7 +4,7 @@
     conditions.isLaptop = true;
 
     boot.initrd.systemd.enable = true;
-    boot.kernelParams = [ "amdgpu.cwsr_enable=0" ];
+    boot.kernelParams = [];
     # boot.kernelPackages = pkgs.linuxPackages_zen;
 
     environment.systemPackages = with pkgs; [
