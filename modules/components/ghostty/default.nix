f9203151fcdb7a83264da17f6746a4690bf73f1f
Thammachart <1731496+Thammachart@users.noreply.github.com>
2026-03-11 04:52:49 +0700

    ghostty keybind

--- modules/components/ghostty/default.nix
+++ modules/components/ghostty/default.nix
@@ -12,13 +12,20 @@
       enable = true;
       systemd.enable = true;
 
+      clearDefaultKeybinds = true;
+
       settings = {
         theme = "Ayu";
         font-family = s.fonts.latin.terminal_monospace;
         font-size = 13;
 
+
+        shell-integration = "detect";
+
         quit-after-last-window-closed = true;
         quit-after-last-window-closed-delay = "10m";
+
+        keybind = "ctrl+l=reset";
       };
     };
   };
