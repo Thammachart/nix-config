9feaeab927638cc67243b1392e33e63cf03c2675
Thammachart <1731496+Thammachart@users.noreply.github.com>
2026-03-15 12:28:54 +0700

    yaml for ironbar

--- modules/components/ironbar/default.nix
+++ modules/components/ironbar/default.nix
@@ -8,7 +8,7 @@
     settings = config.configData.homeSettings // { conditions = osConfig.conditions; };
   in
   {
-    home.file.".config/ironbar/hyprland.toml".source = config.templateFile "ironbar-hyprland-toml" ./hyprland.toml.tmpl settings;
+    home.file.".config/ironbar/hyprland.yaml".source = config.templateFile "ironbar-hyprland-yaml" ./hyprland.yaml.tmpl settings;
     home.file.".config/ironbar/hyprland.css".source = config.templateFile "ironbar-hyprland-css" ./hyprland.css.tmpl settings;
   };
 }
